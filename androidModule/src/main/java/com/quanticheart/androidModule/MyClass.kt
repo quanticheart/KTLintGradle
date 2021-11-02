987946851f92f6aeb123821eb8ef64d2e47e1fb6
Jonn Alves <jonatas.alsantos@jsl.com.br>
2021-11-02 14:52:15 -0300

    Initial commit

---
+++ androidModule/src/main/java/com/quanticheart/androidModule/MyClass.kt
@@ -0,0 +1,10 @@
+package com.quanticheart.androidModule
+
+class MyClass
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/quanticheart/ktlint/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.quanticheart.ktlint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Date()
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+
+
